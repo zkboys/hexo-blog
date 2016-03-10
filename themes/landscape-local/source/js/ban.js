d63b9dd42c2b4f4be0849b4e1809a2e37b5e6e1c
wangshubin <wangshubin@zkboys.com>
2016-03-10 20:50:46 +0800

    blog update

--- themes/landscape-local/source/js/ban.js
+++ themes/landscape-local/source/js/ban.js
@@ -1,28 +1,21 @@
 $(function () {
     //如果是移动端，不添加动画
-    var browser = {
-        versions: function () {
-            var u = navigator.userAgent, app = navigator.appVersion;
-            return {//移动终端浏览器版本信息
-                trident: u.indexOf('Trident') > -1, //IE内核
-                presto: u.indexOf('Presto') > -1, //opera内核
-                webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
-                gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
-                mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
-                ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
-                android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
-                iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
-                iPad: u.indexOf('iPad') > -1, //是否iPad
-                webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
-            };
-        }(),
-        language: (navigator.browserLanguage || navigator.language).toLowerCase()
-    };
-
-    if (browser.versions.mobile || browser.versions.ios || browser.versions.android ||
-        browser.versions.iPhone || browser.versions.iPad) {
+    function IsPC() {
+        var userAgentInfo = navigator.userAgent;
+        var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");
+        var flag = true;
+        for (var v = 0; v < Agents.length; v++) {
+            if (userAgentInfo.indexOf(Agents[v]) > 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+    if(!IsPC()){
         return;
     }
+
     var canvas = document.querySelector('#ban'),
         ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
