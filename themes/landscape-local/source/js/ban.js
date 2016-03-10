a9e5df3c1969591e3a5b713c90a7bd72619f1411
wangshubin <wangshubin@zkboys.com>
2016-03-10 20:41:49 +0800

    blog update

--- themes/landscape-local/source/js/ban.js
+++ themes/landscape-local/source/js/ban.js
@@ -1,145 +1,169 @@
-$(function(){
-  var canvas = document.querySelector('#ban'),
-      ctx = canvas.getContext('2d')
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  ctx.lineWidth = .3;
-  ctx.strokeStyle = (new Color(150)).style;
+$(function () {
+    //如果是移动端，不添加动画
+    var browser = {
+        versions: function () {
+            var u = navigator.userAgent, app = navigator.appVersion;
+            return {//移动终端浏览器版本信息
+                trident: u.indexOf('Trident') > -1, //IE内核
+                presto: u.indexOf('Presto') > -1, //opera内核
+                webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+                gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+                mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+                ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+                android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
+                iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+                iPad: u.indexOf('iPad') > -1, //是否iPad
+                webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+            };
+        }(),
+        language: (navigator.browserLanguage || navigator.language).toLowerCase()
+    };
 
-  var mousePosition = {
-    x: 30 * canvas.width / 100,
-    y: 30 * canvas.height / 100
-  };
-
-  var dots = {
-    nb: 250,
-    distance: 100,
-    d_radius: 150,
-    array: []
-  };
-
-  function colorValue(min) {
-    return Math.floor(Math.random() * 255 + min);
-  }
-  
-  function createColorStyle(r,g,b) {
-    return 'rgba(' + r + ',' + g + ',' + b + ', 0.8)';
-  }
-  
-  function mixComponents(comp1, weight1, comp2, weight2) {
-    return (comp1 * weight1 + comp2 * weight2) / (weight1 + weight2);
-  }
-  
-  function averageColorStyles(dot1, dot2) {
-    var color1 = dot1.color,
-        color2 = dot2.color;
-    
-    var r = mixComponents(color1.r, dot1.radius, color2.r, dot2.radius),
-        g = mixComponents(color1.g, dot1.radius, color2.g, dot2.radius),
-        b = mixComponents(color1.b, dot1.radius, color2.b, dot2.radius);
-    return createColorStyle(Math.floor(r), Math.floor(g), Math.floor(b));
-  }
-  
-  function Color(min) {
-    min = min || 0;
-    this.r = colorValue(min);
-    this.g = colorValue(min);
-    this.b = colorValue(min);
-    this.style = createColorStyle(this.r, this.g, this.b);
-  }
-
-  function Dot(){
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-
-    this.vx = -.5 + Math.random();
-    this.vy = -.5 + Math.random();
-
-    this.radius = Math.random() * 2;
-
-    this.color = new Color();
-    console.log(this);
-  }
-
-  Dot.prototype = {
-    draw: function(){
-      ctx.beginPath();
-      ctx.fillStyle = this.color.style;
-      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      ctx.fill();
+    if (browser.versions.mobile || browser.versions.ios || browser.versions.android ||
+        browser.versions.iPhone || browser.versions.iPad) {
+        return;
     }
-  };
+    var canvas = document.querySelector('#ban'),
+        ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.lineWidth = .3;
+    ctx.strokeStyle = (new Color(150)).style;
 
-  function createDots(){
-    for(i = 0; i < dots.nb; i++){
-      dots.array.push(new Dot());
+    var mousePosition = {
+        x: 30 * canvas.width / 100,
+        y: 30 * canvas.height / 100
+    };
+
+    var dots = {
+        nb: 250,
+        distance: 100,
+        d_radius: 150,
+        array: []
+    };
+
+    function colorValue(min) {
+        return Math.floor(Math.random() * 255 + min);
     }
-  }
 
-  function moveDots() {
-    for(i = 0; i < dots.nb; i++){
-
-      var dot = dots.array[i];
-
-      if(dot.y < 0 || dot.y > canvas.height){
-        dot.vx = dot.vx;
-        dot.vy = - dot.vy;
-      }
-      else if(dot.x < 0 || dot.x > canvas.width){
-        dot.vx = - dot.vx;
-        dot.vy = dot.vy;
-      }
-      dot.x += dot.vx;
-      dot.y += dot.vy;
+    function createColorStyle(r, g, b) {
+        return 'rgba(' + r + ',' + g + ',' + b + ', 0.8)';
     }
-  }
 
-  function connectDots() {
-    for(i = 0; i < dots.nb; i++){
-      for(j = 0; j < dots.nb; j++){
-        i_dot = dots.array[i];
-        j_dot = dots.array[j];
+    function mixComponents(comp1, weight1, comp2, weight2) {
+        return (comp1 * weight1 + comp2 * weight2) / (weight1 + weight2);
+    }
 
-        if((i_dot.x - j_dot.x) < dots.distance && (i_dot.y - j_dot.y) < dots.distance && (i_dot.x - j_dot.x) > - dots.distance && (i_dot.y - j_dot.y) > - dots.distance){
-          if((i_dot.x - mousePosition.x) < dots.d_radius && (i_dot.y - mousePosition.y) < dots.d_radius && (i_dot.x - mousePosition.x) > - dots.d_radius && (i_dot.y - mousePosition.y) > - dots.d_radius){
+    function averageColorStyles(dot1, dot2) {
+        var color1 = dot1.color,
+            color2 = dot2.color;
+
+        var r = mixComponents(color1.r, dot1.radius, color2.r, dot2.radius),
+            g = mixComponents(color1.g, dot1.radius, color2.g, dot2.radius),
+            b = mixComponents(color1.b, dot1.radius, color2.b, dot2.radius);
+        return createColorStyle(Math.floor(r), Math.floor(g), Math.floor(b));
+    }
+
+    function Color(min) {
+        min = min || 0;
+        this.r = colorValue(min);
+        this.g = colorValue(min);
+        this.b = colorValue(min);
+        this.style = createColorStyle(this.r, this.g, this.b);
+    }
+
+    function Dot() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+
+        this.vx = -.5 + Math.random();
+        this.vy = -.5 + Math.random();
+
+        this.radius = Math.random() * 2;
+
+        this.color = new Color();
+        console.log(this);
+    }
+
+    Dot.prototype = {
+        draw: function () {
             ctx.beginPath();
-            ctx.strokeStyle = averageColorStyles(i_dot, j_dot);
-            ctx.moveTo(i_dot.x, i_dot.y);
-            ctx.lineTo(j_dot.x, j_dot.y);
-            ctx.stroke();
-            ctx.closePath();
-          }
+            ctx.fillStyle = this.color.style;
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+            ctx.fill();
         }
-      }
+    };
+
+    function createDots() {
+        for (i = 0; i < dots.nb; i++) {
+            dots.array.push(new Dot());
+        }
     }
-  }
 
-  function drawDots() {
-    for(i = 0; i < dots.nb; i++){
-      var dot = dots.array[i];
-      dot.draw();
+    function moveDots() {
+        for (i = 0; i < dots.nb; i++) {
+
+            var dot = dots.array[i];
+
+            if (dot.y < 0 || dot.y > canvas.height) {
+                dot.vx = dot.vx;
+                dot.vy = -dot.vy;
+            }
+            else if (dot.x < 0 || dot.x > canvas.width) {
+                dot.vx = -dot.vx;
+                dot.vy = dot.vy;
+            }
+            dot.x += dot.vx;
+            dot.y += dot.vy;
+        }
     }
-  }
 
-  function animateDots() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    moveDots();
-    connectDots();
-    drawDots();
+    function connectDots() {
+        for (i = 0; i < dots.nb; i++) {
+            for (j = 0; j < dots.nb; j++) {
+                i_dot = dots.array[i];
+                j_dot = dots.array[j];
 
-    requestAnimationFrame(animateDots);	
-  }
+                if ((i_dot.x - j_dot.x) < dots.distance && (i_dot.y - j_dot.y) < dots.distance && (i_dot.x - j_dot.x) > -dots.distance && (i_dot.y - j_dot.y) > -dots.distance) {
+                    if ((i_dot.x - mousePosition.x) < dots.d_radius && (i_dot.y - mousePosition.y) < dots.d_radius && (i_dot.x - mousePosition.x) > -dots.d_radius && (i_dot.y - mousePosition.y) > -dots.d_radius) {
+                        ctx.beginPath();
+                        ctx.strokeStyle = averageColorStyles(i_dot, j_dot);
+                        ctx.moveTo(i_dot.x, i_dot.y);
+                        ctx.lineTo(j_dot.x, j_dot.y);
+                        ctx.stroke();
+                        ctx.closePath();
+                    }
+                }
+            }
+        }
+    }
 
-  $('canvas').on('mousemove', function(e){
-    mousePosition.x = e.pageX;
-    mousePosition.y = e.pageY;
-  });
+    function drawDots() {
+        for (i = 0; i < dots.nb; i++) {
+            var dot = dots.array[i];
+            dot.draw();
+        }
+    }
 
-  $('canvas').on('mouseleave', function(e){
-    mousePosition.x = canvas.width / 2;
-    mousePosition.y = canvas.height / 2;
-  });
+    function animateDots() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        moveDots();
+        connectDots();
+        drawDots();
 
-  createDots();
-  requestAnimationFrame(animateDots);	
+        requestAnimationFrame(animateDots);
+    }
+
+    $('canvas').on('mousemove', function (e) {
+        mousePosition.x = e.pageX;
+        mousePosition.y = e.pageY;
+    });
+
+    $('canvas').on('mouseleave', function (e) {
+        mousePosition.x = canvas.width / 2;
+        mousePosition.y = canvas.height / 2;
+    });
+
+    createDots();
+    requestAnimationFrame(animateDots);
 });
