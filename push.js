1e98bc4a4289a921f740fad42c75bcf1d4aefd66
wangshubin <wangshubin@xiaoyage.com>
2016-04-13 17:33:15 +0800

    blog update

--- push.js
+++ push.js
@@ -11,11 +11,14 @@ if (!program.message) {
     console.error('ERROR:请输入注释！！！');
     return
 }
+// 每次提交修改版本号
+/*
 var packageJson = JSON.parse(fs.readFileSync('./package.json'));
 var vs = packageJson.version.split('.').map(Number);
 var newVersion = vs[0] + '.' + vs[1] + '.' + (vs[2] + 1);
 packageJson.version = newVersion;
 fs.writeFileSync('./package.json', JSON.stringify(packageJson));
+*/
 exec("git add .", function (error, stdout, stderr) {
     stdout && console.log(stdout);
     stderr && console.log(stderr);
